7d132f11644d7472ebb0b41af52e831e802cd5ee
GruzinHW <lech.cirmirakis@go2.pl>
2017-10-10 12:19:17 +0200

    add moveDog method

--- js/app.js
+++ js/app.js
@@ -18,6 +18,16 @@ function Game() { // C
     this.index = function(x, y) {
         return x + (y * 10);
     }
+
+    var self = this;
+
+    this.startGame = function() {
+        this.showDog();
+        this.showDainty();
+        this.idSetInterval = setInterval(function() {
+            self.moveDog();
+        }, 500);
+    }
 }
 
 Game.prototype.showDog = function() {
@@ -28,14 +38,22 @@ Game.prototype.showDainty = function() {
     var da = this.board[this.index(this.dainty.x, this.dainty.y)].classList.add("dainty");
 };
 
-Game.prototype.startGame = function() {
-    this.idSetInterval = setInterval(function() {
-        console.log("hura z setIntervala");
-    });
-
+Game.prototype.moveDog = function() {
+    if (this.dog.direction === "right") {
+        this.dog.x += 1;
     }
+    else if (this.dog.direction === "down") {
+        this.dog.y -= 1;
+    }
+    else if (this.dog.direction === "up") {
+        this.dog.y += 1;
+    }
+    else if (this.dog.direction === "left") {
+        this.dog.x -= 1;
+    }
+    this.showDog();
+}
 
 var gra = new Game();
+
 gra.startGame();
-gra.showDog();
-gra.showDainty();
